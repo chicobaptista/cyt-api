158acc6822707038dc82fce3f669ad0b091ba8fc
fbaptista <chico.baptista@gmail.com>
2022-07-16 15:59:53 -0300

    test(create-theme): add return test case

--- src/domains/theme/use-cases/create-theme.spec.ts
+++ src/domains/theme/use-cases/create-theme.spec.ts
@@ -1,8 +1,8 @@
+import { ThemeDTO, ThemeProps } from '@theme/entities/theme.interface'
 import chai, { expect } from 'chai'
 import { CCreateTheme } from './create-theme'
 import { CreateTheme } from './use-cases.interface'
 import { CreateThemeRepository } from '@theme/repositories/theme.repository.interface'
-import { ThemeProps } from '@theme/entities/theme.interface'
 import { generateMockThemeProps } from '@theme/entities/theme.utils.spec'
 import { makeMockCreateThemeRepository } from '@theme/repositories/theme.repository.utils.spec'
 import sinonChai from 'sinon-chai'
@@ -16,6 +16,33 @@ describe('Create Theme use case', () => {
             const createThemeProps: ThemeProps = generateMockThemeProps()
             await createTheme.create(createThemeProps)
             expect(createRepo.saveTheme).to.have.been.called
+        })
+        it('should return a theme dto', async () => {
+            const createRepo: CreateThemeRepository =
+                makeMockCreateThemeRepository()
+            const createTheme: CreateTheme = new CCreateTheme(createRepo)
+            const createThemeProps: ThemeProps = generateMockThemeProps()
+            const createResult: ThemeDTO = await createTheme.create(
+                createThemeProps,
+            )
+            expect(createResult, 'should have all props').to.deep.contain(
+                createThemeProps,
+            )
+            expect(
+                createResult,
+                'should have createdAt property',
+            ).to.haveOwnProperty('createdAt')
+            expect(
+                createResult,
+                'should have updatedAt property',
+            ).to.haveOwnProperty('updatedAt')
+            expect(createResult, 'should have id property').to.haveOwnProperty(
+                'id',
+            )
+            expect(
+                createResult.updatedAt,
+                'should have a createdAt equal to updatedAt',
+            ).to.deep.equal(createResult.createdAt)
         })
     })
 })
