8a6c97ed6a75d391b6046b3a86879f094fbabe4c
fbaptista <chico.baptista@gmail.com>
2022-07-17 20:02:57 -0300

    docs(repository-dbdriver): adds documentation to interface methods

--- src/shared/drivers/db.driver.interface.ts
+++ src/shared/drivers/db.driver.interface.ts
@@ -2,7 +2,27 @@ import { BaseDTO } from '@shared/entities/entity.interface'
 
 /* eslint-disable no-unused-vars */
 export interface DbDriver<T extends BaseDTO> {
+    /**
+     * Retrieves a {@link BaseDTO} object from a Db.
+     * @param  {string} id
+     * @returns Promise
+     * @fulfill {@link BaseDTO}
+     */
     findOne(id: string): Promise<T>
+
+    /**
+     * Saves a {@link BaseDTO} object to a Db.
+     * @param  {T} data
+     * @returns Promise
+     * @fulfill {@link BaseDTO}
+     */
     save(data: T): Promise<T>
+
+    /**
+     * Updates a {@link BaseDTO} object to a Db.
+     * @param  {T} data
+     * @returns Promise
+     * @fulfill {@link BaseDTO}
+     */
     update(data: T): Promise<T>
 }
