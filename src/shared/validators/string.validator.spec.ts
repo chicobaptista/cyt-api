e96e0805e0370faf43e212e91544461423857bf3
fbaptista <chico.baptista@gmail.com>
2022-10-16 16:09:12 -0300

    feat(validator): implements empty string and uuid string validators

--- src/shared/validators/string.validator.spec.ts
+++ src/shared/validators/string.validator.spec.ts
@@ -1,5 +1,6 @@
+import { isNonEmptyString, isString, isUuid } from './string.validator'
 import { expect } from 'chai'
-import { isString } from './string.validator'
+import { v4 } from 'uuid'
 
 describe('isString', () => {
     it('should return true if argument is string literal', () => {
@@ -23,10 +24,98 @@ describe('isString', () => {
     it('should return false if argument is a boolean', () => {
         expect(isString(true)).to.be.false
     })
+    it('should return true if argument is a literal string boolean', () => {
+        expect(isString('false')).to.be.true
+    })
     it('should return false if argument is null', () => {
         expect(isString(null)).to.be.false
     })
+    it('should return true if argument is a literal string null', () => {
+        expect(isString('null')).to.be.true
+    })
     it('should return false if argument is undefined', () => {
         expect(isString(undefined)).to.be.false
+    })
+    it('should return true if argument is a literal string undefined', () => {
+        expect(isString('undefined')).to.be.true
+    })
+})
+
+describe('isNonEmptyString', () => {
+    it('should return true if argument is string literal', () => {
+        expect(isNonEmptyString('foo')).to.be.true
+    })
+    it('should return true if argument is a constructed String', () => {
+        expect(isNonEmptyString(new String('foo'))).to.be.true
+    })
+    it('should return false if argument is a number', () => {
+        expect(isNonEmptyString(1)).to.be.false
+    })
+    it('should return false if argument is a Date', () => {
+        expect(isNonEmptyString(new Date())).to.be.false
+    })
+    it('should return false if argument is an array', () => {
+        expect(isNonEmptyString(['foo'])).to.be.false
+    })
+    it('should return false if argument is an object', () => {
+        expect(isNonEmptyString({ foo: 'bar' })).to.be.false
+    })
+    it('should return false if argument is a boolean', () => {
+        expect(isNonEmptyString(true)).to.be.false
+    })
+    it('should return false if argument is a literal string true boolean', () => {
+        expect(isNonEmptyString('true')).to.be.true
+    })
+    it('should return false if argument is a literal string false boolean', () => {
+        expect(isNonEmptyString('false')).to.be.false
+    })
+    it('should return false if argument is null', () => {
+        expect(isNonEmptyString(null)).to.be.false
+    })
+    it('should return false if argument is a literal string null', () => {
+        expect(isNonEmptyString('null')).to.be.false
+    })
+    it('should return false if argument is undefined', () => {
+        expect(isNonEmptyString(undefined)).to.be.false
+    })
+    it('should return false if argument is a literal string undefined', () => {
+        expect(isNonEmptyString('undefined')).to.be.false
+    })
+})
+
+describe('isUuid', () => {
+    it('should return true if argument is a valid uuid string literal', () => {
+        expect(isUuid(v4())).to.be.true
+    })
+    it('should return true if argument is a valid uuid constructed String', () => {
+        expect(isUuid(new String(v4()))).to.be.true
+    })
+
+    it('should return false if argument is a non-uuid string literal', () => {
+        expect(isUuid('a24a6ea4-ce75-4665-a070')).to.be.false
+    })
+    it('should return false if argument is a non-uuid constructed String', () => {
+        expect(isUuid(new String('a24a6ea4-ce75-4665-a070'))).to.be.false
+    })
+    it('should return false if argument is a number', () => {
+        expect(isUuid(1)).to.be.false
+    })
+    it('should return false if argument is a Date', () => {
+        expect(isUuid(new Date())).to.be.false
+    })
+    it('should return false if argument is an array', () => {
+        expect(isUuid(['foo'])).to.be.false
+    })
+    it('should return false if argument is an object', () => {
+        expect(isUuid({ foo: 'bar' })).to.be.false
+    })
+    it('should return false if argument is a boolean', () => {
+        expect(isUuid(true)).to.be.false
+    })
+    it('should return false if argument is null', () => {
+        expect(isUuid(null)).to.be.false
+    })
+    it('should return false if argument is undefined', () => {
+        expect(isUuid(undefined)).to.be.false
     })
 })
--- src/shared/validators/string.validator.ts
+++ src/shared/validators/string.validator.ts
@@ -1,3 +1,17 @@
 export function isString(subject: any): boolean {
     return typeof subject === 'string' || subject instanceof String
 }
+
+export function isNonEmptyString(subject: any): boolean {
+    const emptyStrings = ['', 'null', 'undefined', 'false']
+    return (
+        isString(subject) && emptyStrings.findIndex((e) => e === subject) === -1
+    )
+}
+
+export function isUuid(subject: any): boolean {
+    const regexExp =
+        /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi
+
+    return isString(subject) && regexExp.test(subject)
+}
