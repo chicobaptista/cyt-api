063272d603c6aa312b06e09ca4f0edab225055ea
fbaptista <chico.baptista@gmail.com>
2022-10-16 16:31:54 -0300

    refactor(mongo): implements mongo host variable for different hostnames

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -6,11 +6,13 @@ dotenv.config()
 const MONGO_USER = env.get('MONGO_USER').required().asString()
 const MONGO_PWD = env.get('MONGO_PWD').required().asString()
 const MONGO_DB = env.get('MONGO_DB').required().asString()
+const MONGO_HOST = env.get('MONGO_HOST').required().asString()
 
 export const mongoConfig = {
     user: MONGO_USER,
     password: MONGO_PWD,
     database: MONGO_DB,
+    host: MONGO_HOST,
 }
 
 const APP_PORT = env.get('APP_PORT').default(3000).asIntPositive()
--- src/shared/drivers/index.ts
+++ src/shared/drivers/index.ts
@@ -3,8 +3,8 @@ import { CMongoDbDriver } from '@shared/drivers/mongo-db.driver'
 import { mongoConfig } from '@shared/config'
 import mongoose from 'mongoose'
 
-const { user, password, database } = mongoConfig
-mongoose.connect(`mongodb://${user}:${password}@localhost:27017/${database}`)
+const { user, password, database, host } = mongoConfig
+mongoose.connect(`mongodb://${user}:${password}@${host}:27017/${database}`)
 function makeDbDriver<T extends BaseDTO>(collectionName, schema) {
     return new CMongoDbDriver<T>(collectionName, schema)
 }
